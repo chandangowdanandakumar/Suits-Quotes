57faefd420f2eb6f3151a15c9abbf3d5ce91bdc9
chandangowdanandakumar <chandangowdanandakumar@outlook.com>
2021-10-13 22:55:58 -0700

    component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddQuoteComponent } from './add-quote/add-quote.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'add-quote',
+    component: AddQuoteComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   quotesList = [1,2,3,4,5,6,7,8,9,10]
+
+  constructor( private router:Router){}
+  navtoAddNote(){
+    this.router.navigateByUrl('add-quote')
+  }
 }
